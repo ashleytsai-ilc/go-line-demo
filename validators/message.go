a64218f19d85a4c9a6b9795ef5a171fba8942d6d
ashley <o60431y@gmail.com>
2022-06-13 13:27:22 +0800

    feat(validators.message): add push message router params validator

---
+++ validators/message.go
@@ -0,0 +1,16 @@
+package validators
+
+import (
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type PushMessage struct {
+	UserID  string `json:"userId" binding:"required,min=20,max=80"`
+	Content string `json:"content" binding:"required,min=1,trim"`
+}
+
+var Trim validator.Func = func(fl validator.FieldLevel) bool {
+	return strings.Trim(fl.Field().String(), " ") != ""
+}
