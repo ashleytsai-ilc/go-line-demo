fd57eced32df16eabdafb9d1d2588174f8e1489f
ashley <o60431y@gmail.com>
2022-06-13 14:14:04 +0800

    refactor(utils.linebot): new linebot only once

--- utils/linebot.go
+++ utils/linebot.go
@@ -7,15 +7,20 @@ import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
 )
 
-func GetLinebot() *linebot.Client {
+var linebotClient *linebot.Client
+
+func NewLinebot() {
 	settings := config.GetConfig()
 	channelSecret := settings.GetString("LINE_CHANNEL_SECRET")
 	channelAccess := settings.GetString("LINE_CHANNEL_ACCESS")
 	client := &http.Client{}
-	bot, err := linebot.New(channelSecret, channelAccess, linebot.WithHTTPClient(client))
+	var err error
+	linebotClient, err = linebot.New(channelSecret, channelAccess, linebot.WithHTTPClient(client))
 	if err != nil {
 		panic(err)
 	}
+}
 
-	return bot
+func GetLinebot() *linebot.Client {
+	return linebotClient
 }
