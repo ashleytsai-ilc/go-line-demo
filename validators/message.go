5647e38bda3fc74833c4f623762e3ff2af566c0c
ashley <o60431y@gmail.com>
2022-06-13 14:10:04 +0800

    feat(validators.message): add get messages router param validator

--- validators/message.go
+++ validators/message.go
@@ -11,6 +11,10 @@ type PushMessage struct {
 	Content string `json:"content" binding:"required,min=1,trim"`
 }
 
+type GetMessages struct {
+	UserID string `uri:"userid" binding:"required,min=20,max=80"`
+}
+
 var Trim validator.Func = func(fl validator.FieldLevel) bool {
 	return strings.Trim(fl.Field().String(), " ") != ""
 }
